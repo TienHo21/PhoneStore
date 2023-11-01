f3a25ff6155b8363b497a1afbd5cde8568cdf938
TienHo21 <hotuyen21092004@gmail.com>
2023-11-01 21:57:07 +0700

    init project

---
+++ src/demo_git/Demo_git.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package demo_git;
+
+/**
+ *
+ * @author LENOVO
+ */
+public class Demo_git {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.err.println("Demo git lỗi quài cay thế nhờ");
+    }
+    
+}
